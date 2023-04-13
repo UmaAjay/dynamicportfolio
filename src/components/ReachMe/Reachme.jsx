19f7539afbaf72063b502bfc606d0e26c94ee765
UMA AJAY KUMAR REDDY P S <umaveena.alvas@gmail.com>
2023-04-13 14:19:43 +0530

    initial-commit

---
+++ src/components/ReachMe/Reachme.jsx
@@ -0,0 +1,26 @@
+import "./reachme.css";
+
+const ReachMe = () => {
+  return (
+    <>
+      <section className="reachme">
+        <div className="reachme-container">
+          <div className="reachme-info">
+            <h1 className="reachme-title">
+              <span> You have Fresh Ideas,</span> <br />
+              Wanna Reach Me out or just want to say hi.
+            </h1>
+            <div className="contact-by">
+              <p>
+              contact me
+              </p>
+              <button className="reachout-btn">Reach Out </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ReachMe;
